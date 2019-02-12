5add2e968026a85b5278e553ed4fccc6037b36cf
Lic. Edgar Zuniga <Lic. Edgar Zuniga@EDGAR>
2019-02-11 23:31:29 -0600

    Agregar Pagina Productos

---
+++ QuieroPizza/Quieropizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using Quieropizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quieropizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza 6 Quesos";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza Chesse Bytes";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza Super Suprema";
+
+            var listaproductos = new List<ProductoModel>();
+            listaproductos.Add(producto1);
+            listaproductos.Add(producto2);
+            listaproductos.Add(producto3);
+
+            return View(listaproductos);
+        }
+    }
+}
